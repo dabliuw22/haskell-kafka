a0b850141b19e04d5fed62b1dc72228143b9ea7a
Will Leyton <wleylop22@gmail.com>
2020-11-11 13:34:45 -0500

    Run producer

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Adapter.Publisher (useProducer)
+import Domain.Message (Key(..), Message(..))
+
 main :: IO ()
-main = print "Hello Kafka"
+main = do
+  let key = Key "my-key"
+      message = Message "message"
+  useProducer key message
+  
