e37ff07d9f47d73d32dcb39492373e83bd0baf18
Will Leyton <wleylop22@gmail.com>
2020-11-11 13:34:26 -0500

    Add useProducer

--- src/Adapter/Publisher.hs
+++ src/Adapter/Publisher.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Adapter.Publisher where
+module Adapter.Publisher (useProducer) where
 
+import Control.Exception (bracket)
 import Data.Map (Map, fromList)
 import Data.Text (Text)
+import Domain.Message (Key(..), Message(..))
 import Kafka.Producer
 
 extraProperties :: Map Text Text
@@ -23,3 +25,35 @@ producerProperties =
     <> logLevel KafkaLogInfo
     <> compression Snappy
     <> extraProps extraProperties
+
+topic :: TopicName
+topic = TopicName "haskell.t"
+
+makeRecord :: Key -> Message -> ProducerRecord
+makeRecord (Key k) (Message m) =
+  ProducerRecord {
+    prTopic = topic,
+    prPartition = UnassignedPartition,
+    prKey = Just k,
+    prValue = Just m
+  }
+
+useProducer :: Key -> Message -> IO ()
+useProducer key message =
+  bracket acquire release use >>= print
+  where
+    acquire = newProducer producerProperties
+    release (Left _) = pure ()
+    release (Right prod) = closeProducer prod
+    use (Left e) = pure $ Left e
+    use (Right prod) = send prod key message
+
+
+send :: KafkaProducer -> Key -> Message -> IO (Either KafkaError ())
+send producer key message = do
+  let record = makeRecord key message
+  result <- produceMessage producer record
+  case result of
+    Just e -> return $ Left e
+    _      -> return $ Right ()
+
