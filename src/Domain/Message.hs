4ea8f6f2d8e05f9711550ed458036002c5d5b3c8
Will Leyton <wleylop22@gmail.com>
2020-11-19 11:25:33 -0500

    Add Event

--- src/Domain/Message.hs
+++ src/Domain/Message.hs
@@ -1,7 +1,12 @@
-module Domain.Message (Message (..), Key (..)) where
+module Domain.Message (Message (..), Key (..), Event (..)) where
 
 import Data.ByteString (ByteString)
 
 newtype Key = Key ByteString
 
 newtype Message = Message ByteString
+
+data Event
+  = OneEvent {value :: String}
+  | OtherEvent {number :: Int}
+  deriving (Show)
